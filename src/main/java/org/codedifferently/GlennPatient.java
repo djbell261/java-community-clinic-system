40108cbb71326416a714bb8ced153889635069d2
glenntyson <gatyson261@gmail.com>
2026-02-19 22:15:25 -0500

    Added setter and getters to the GlennPatitent class

--- src/main/java/org/codedifferently/GlennPatient.java
+++ src/main/java/org/codedifferently/GlennPatient.java
@@ -1,4 +1,32 @@
 package org.codedifferently;
 
 public class GlennPatient {
+    private int idCounter = 1;
+    private int id;
+    private String name;
+    private boolean checkedIn;
+
+    public void glennPatient(String name) {
+        this.id = idCounter++;
+        this.name = name;
+        this.checkedIn = false;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean isCheckedIn(){
+        return checkedIn;
+    }
+    public void checkIn(){
+        this.checkedIn =true;
+    }
+    public String toString(){
+        return "ID: " +id +" | Name: " +name+"| Checked In: " + checkedIn;
+    }
+
 }
